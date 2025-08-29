f25bae2159db279fe0a1557a06ba91c59d4e1422
piyusha05 <piyushabhadange9@gmail.com>
2025-08-29 18:20:22 +0530

    Added contact oage and added login and register button to navbar and improved design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
+      {/* Left: Logo */}
       <div className="logo">CareCure</div>
-      <ul className="nav-links">
+
+      {/* Middle: Main navigation links */}
+      <ul className="nav-links center-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/departments">Departments</Link></li>
         <li><Link to="/doctors">Doctors</Link></li>
         <li><Link to="/appointment">Appointment</Link></li>
         <li><Link to="/contact">Contact</Link></li>
+      </ul>
+
+      {/* Right: Login & Register */}
+      <ul className="nav-links auth-links">
+        <li className="auth-link"><Link to="/login">Login</Link></li>
+        <li className="auth-link"><Link to="/register">Register</Link></li>
       </ul>
     </nav>
   );
